a700df9f1764c6ba7a6b848da7fae91f92a141f6
v-venturi <v.venturi@yahoo.com.br>
2021-08-09 22:06:41 -0300

    Initial commit

---
+++ src/main/java/com/controle/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.controle.repository;
+
+import com.controle.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
---
+++ src/main/java/com/controle/service/JornadaService.java
@@ -0,0 +1,43 @@
+package com.controle.service;
+
+import com.controle.model.JornadaTrabalho;
+import com.controle.repository.JornadaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JornadaService {
+
+
+    JornadaRepository jornadaRepository;
+
+    @Autowired
+    public JornadaService(JornadaRepository jornadaRepository) {
+        this.jornadaRepository = jornadaRepository;
+    }
+
+    public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho){
+       return jornadaRepository.save(jornadaTrabalho);
+
+    }
+
+    public List<JornadaTrabalho> findAll() {
+       return   jornadaRepository.findAll();
+    }
+
+    public Optional<JornadaTrabalho> getById(Long idJornada) {
+        return jornadaRepository.findById(idJornada);
+    }
+
+    public JornadaTrabalho updateJornada(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+
+    }
+
+    public void deleteJornada(Long idJornada) {
+        jornadaRepository.deleteById(idJornada);
+    }
+}
